e83d65998563b43b246da009b1fe35d7ae5a3f00
Mainur <kentykimainur@gmail.com>
2026-09-19 15:26:44 +0600

    fix: prepare backend for production

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -47,7 +47,7 @@ export class AuthController {
     res.cookie('refreshToken', tokens.refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +9,8 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
   });
 
   app.setGlobalPrefix('api');
